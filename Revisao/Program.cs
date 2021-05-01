5c91240dbd4983b4e06329fe99e891f7fc432a0e
dan-primo <danielly.primo@gmail.com>
2021-05-01 14:42:54 -0300

    praticando-primeiros-passos

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,23 +6,42 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Infome a opção desejada:");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine();
-
-            string opcaoUsuario = Console.ReadLine();
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: implement adicionar alunos
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
-                        //TODO: implement listar alunos
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
                         break;
                     case "3":
                         //TODO: implement calcular media geral
@@ -30,7 +49,24 @@ namespace Revisao
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
+
+                opcaoUsuario = ObterOpcaoUsuario();
             }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Infome a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine("-----------------------------");
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine("-----------------------------");
+            return opcaoUsuario;
         }
     }
 }
