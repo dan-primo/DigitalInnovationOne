ac236ec6f4cf07cb6a470bf786b07972a15a36e6
dan-primo <danielly.primo@gmail.com>
2021-05-01 13:00:08 -0300

    revisao-introdutoria

---
+++ Revisao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Infome a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: implement adicionar alunos
+                        break;
+                    case "2":
+                        //TODO: implement listar alunos
+                        break;
+                    case "3":
+                        //TODO: implement calcular media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
